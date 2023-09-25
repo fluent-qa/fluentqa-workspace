9c61cb1c66d4d43caa39c6597dc67ddb3137b8f8
patrick <patrickwuke@163.com>
2023-09-25 10:19:21 +0800

    init upload file model

---
+++ fluent-erupts/fluent-upload/src/main/java/io/fluentqa/proxy/UploadFileTypeEnum.java
@@ -0,0 +1,14 @@
+//package io.fluentqa.proxy;
+//
+//public enum UploadFileTypeEnum {
+//    EXCEL_TC,FREEMIND,PM;
+//
+//    public static UploadFileTypeEnum parseType(String uploadFileType) {
+//        for (UploadFileTypeEnum uploadFileTypeEnum : UploadFileTypeEnum.values()) {
+//            if (uploadFileTypeEnum.name().equals(uploadFileType)) {
+//                return uploadFileTypeEnum;
+//            }
+//        }
+//       throw  new RuntimeException("UploadFileTypeEnum is not found for " + uploadFileType);
+//    }
+//}
