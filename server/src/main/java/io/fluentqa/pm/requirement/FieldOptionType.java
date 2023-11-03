16fe66afddc75709ead219f1f9dea698e8ba7549
patrick <patrickwuke@163.com>
2023-11-03 15:27:45 +0800

    test requirement cases

---
+++ server/src/main/java/io/fluentqa/pm/requirement/FieldOptionType.java
@@ -0,0 +1,13 @@
+package io.fluentqa.pm.requirement;
+
+import lombok.Getter;
+
+@Getter
+public enum FieldOptionType {
+    NUMBER("数值"),STRING("字符串"),RELATION("关联"),DATE("日期"),ENUM("枚举"),BOOLEAN("布尔");
+
+    private String desc;
+    FieldOptionType(String desc) {
+        this.desc = desc;
+    }
+}
