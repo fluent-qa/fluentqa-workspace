8bae5dd3230f2d0fa732cd7c7485bf6412b4f873
patrick <patrickwuke@163.com>
2024-01-09 17:15:21 +0800

    update async function

--- qaserver/src/main/java/io/fluentqa/generator/repo/DataSourceTableColumRepo.java
+++ qaserver/src/main/java/io/fluentqa/generator/repo/DataSourceTableColumRepo.java
@@ -5,9 +5,13 @@ import io.fluentqa.generator.model.DataSourceTableColumModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DataSourceTableColumRepo extends JpaRepository<DataSourceTableColumModel, Long>,
         JpaSpecificationExecutor<Long> {
+
+    @Async
+    public <S extends DataSourceTableColumModel> S save(S entity);
 }
