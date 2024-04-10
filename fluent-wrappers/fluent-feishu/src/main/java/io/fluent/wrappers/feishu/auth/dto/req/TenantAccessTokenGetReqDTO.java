9d086475644c808b5515e4a2bb912c3324f643f0
patrick <patrickwuke@163.com>
2024-04-10 16:27:48 +0800

    add fluent-wrappers

---
+++ fluent-wrappers/fluent-feishu/src/main/java/io/fluent/wrappers/feishu/auth/dto/req/TenantAccessTokenGetReqDTO.java
@@ -0,0 +1,29 @@
+package io.fluent.wrappers.feishu.auth.dto.req;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 企业自建应用token获取请求参数
+
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class TenantAccessTokenGetReqDTO {
+
+    /**
+     * 应用ID
+     */
+    private String appId;
+
+    /**
+     * 密钥
+     */
+    private String appSecret;
+
+}
---
+++ fluent-wrappers/fluent-feishu/src/main/java/io/fluent/wrappers/feishu/auth/dto/resp/TenantAccessTokenGetRespDTO.java
@@ -0,0 +1,36 @@
+package io.fluent.wrappers.feishu.auth.dto.resp;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+/**
+ * 企业自建应用token获取请求参数
+ *
+ */
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class TenantAccessTokenGetRespDTO {
+
+    /**
+     * 响应码
+     */
+    private Integer code;
+
+    /**
+     * 消息
+     */
+    private String msg;
+
+    /**
+     * token
+     */
+    private String tenantAccessToken;
+
+    /**
+     * 过期时间
+     */
+    private Integer expire;
+
+
+}
